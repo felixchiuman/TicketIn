80443478ce770657cb709bb8fa6138996ee9543a
chiumanfelix <chiumanfelix@gmail.com>
2022-04-18 21:29:45 +0700

    ver 0.2.1(minor coding type)

--- app/src/main/java/com/felix/ticketin/api/ApiService.kt
+++ app/src/main/java/com/felix/ticketin/api/ApiService.kt
@@ -4,6 +4,7 @@ import com.felix.ticketin.model.comingsoon.GetAllComingSoonResponse
 import com.felix.ticketin.model.playingnow.GetAllPlayingNowIResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("movie/now_playing?api_key=1ff43c35d49d9fd669947e7043b603fc")
@@ -11,4 +12,7 @@ interface ApiService {
 
     @GET("movie/upcoming?api_key=1ff43c35d49d9fd669947e7043b603fc")
     fun getAllComingSoon(): Call<GetAllComingSoonResponse>
+
+//    @GET("movie/{movie_id}")
+//    fun getMovieDetail(@Path("movie_id")movieid:Int): DetailMovieResponse
 }
--- app/src/main/java/com/felix/ticketin/ui/fragment/MovieDetailFragment.kt
+++ app/src/main/java/com/felix/ticketin/ui/fragment/MovieDetailFragment.kt
@@ -1,5 +1,7 @@
 package com.felix.ticketin.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,15 +9,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.felix.ticketin.R
+import com.felix.ticketin.databinding.MainMenuFragmentBinding
+import com.felix.ticketin.databinding.MovieDetailFragmentBinding
+import com.felix.ticketin.firebase.RegisterActivity
 
 class MovieDetailFragment : Fragment() {
+    private var _binding: MovieDetailFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.movie_detail_fragment, container, false)
+        _binding = MovieDetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
 }
