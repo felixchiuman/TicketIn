4cde708e393ac6d5ee4cc6e0e1854069be7c4d8c
chiumanfelix <chiumanfelix@gmail.com>
2022-05-12 17:37:05 +0700

    ver 0.3.2(firebase profile img error)

--- app/src/main/java/com/felix/ticketin/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/felix/ticketin/ui/fragment/ProfileFragment.kt
@@ -1,20 +1,100 @@
 package com.felix.ticketin.ui.fragment
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.felix.ticketin.R
+import com.felix.ticketin.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment() {
+
+    companion object{
+        const val REQUEST_CAMERA = 100
+    }
+
+    private lateinit var imageUri : Uri
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var firebaseAuth : FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadProfile()
+
+        binding.apply {
+            ivBack.setOnClickListener{
+                it.findNavController().navigate(R.id.action_profileFragment_to_navigation_home)
+            }
+            ivProfile.setOnClickListener {
+                intentCamera()
+            }
+        }
+    }
+
+    private fun intentCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            activity?.packageManager?.let {
+                intent.resolveActivity(it).also {
+                    startActivityForResult(intent, REQUEST_CAMERA)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+            uploadImage(imgBitmap)
+        }
+    }
+
+    private fun uploadImage(imgBitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val ref = FirebaseStorage.getInstance().reference.child("img/${FirebaseAuth.getInstance().currentUser?.uid}")
+
+        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val image = baos.toByteArray()
+
+        ref.putBytes(image)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    ref.downloadUrl.addOnCompleteListener {
+                        it.result?.let {
+                            imageUri = it
+                            binding.ivProfile.setImageBitmap(imgBitmap)
+                        }
+                    }
+                }
+            }
+    }
+
+    private fun loadProfile() {
+        firebaseAuth = FirebaseAuth.getInstance()
+        val firebaseUser = firebaseAuth.currentUser
+        val email = firebaseUser?.email
+        binding.tvEmail.text = "Email : ${email}"
+    }
 }
